31f89c35a49718a06336d5fbda05d570c7a90ea3
Neko-Sato <hisatosi35350@gmail.com>
2023-06-22 06:31:13 +0900

    静的ライブラリによるincludeは<>にしようかなぁ....

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 17:25:11 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/06/22 06:27:00 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/06/22 06:27:56 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include <ft_printf.h>
 #include <stdlib.h>
 
 int	main(int argc, char *argv[])
