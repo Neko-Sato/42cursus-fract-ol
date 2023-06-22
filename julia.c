d8a642cc88448a5baef54976e6ef3dba1bdd7ba9
Neko-Sato <hisatosi35350@gmail.com>
2023-06-22 22:12:05 +0900

    フラクタルのアルゴリズム

--- julia.c
+++ julia.c
@@ -6,8 +6,28 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 02:19:08 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/06/22 02:31:55 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/06/22 22:11:43 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Z_(n+1)=Z_n^2+C, C=a+bi
+#include "fractol.h"
+#include "ft_complex.h"
+#include <stdlib.h>
+
+size_t	julia_formula(t_complex z, t_complex c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < MAX_ITER && ft_cabs(z) < 2.)
+	{
+		z = ft_cadd(ft_cpow(z, (t_complex){2, 0}), c);
+		i++;
+	}
+	return (i);
+}
+
+void	julia(t_complex c)
+{
+	exit(0);
+}
--- mandelbrot.c
+++ mandelbrot.c
@@ -6,8 +6,30 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 02:27:32 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/06/22 02:32:06 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/06/22 22:11:34 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Z_(n+1)=Z_n^2+C, Z=0
+#include "fractol.h"
+#include <mlx.h>
+#include <stdlib.h>
+
+size_t	mandelbrot_formula(t_complex c)
+{
+	size_t		i;
+	t_complex	z;
+
+	i = 0;
+	z = (t_complex){0, 0};
+	while (i < MAX_ITER && ft_cabs(z) < 2.)
+	{
+		z = ft_cadd(ft_cpow(z, (t_complex){2, 0}), c);
+		i++;
+	}
+	return (i);
+}
+
+void	mandelbrot(void)
+{
+	exit(0);
+}
